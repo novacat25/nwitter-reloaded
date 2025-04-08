27a772d1b8bcd08bfd5b05bc1d6892ca8ece06c1
NovaCat <sparkstorms2@gmail.com>
2025-04-08 23:25:50 +0900

    Refactor: auth-components.ts

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,58 +1,10 @@
 import { useState } from "react"
-import styled from "styled-components"
 import { INPUT_TYPE_NAME, INPUT_TYPE_EMAIL, INPUT_TYPE_PASSWORD } from "../constants"
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
 import { auth } from "../utils/firebase"
 import { Link, useNavigate } from "react-router-dom"
 import { FirebaseError } from "@firebase/util"
-
-const Wrapper = styled.section`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0;
-`
-
-const Form = styled.form`
-  margin-top: 50px;
-  margin-bottom: 12px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
-  width: 100%;
-`
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-
-  &[type="submit"] {
-     cursor: pointer;
-     &:hover {
-       opacity: 0.8;
-     }
-   }
-`
-const Title = styled.h1`
-  font-size: 42px;
-`
-
-const Error = styled.span`
-  font-weight: 600;
-  color: #ed4848;
- `
-
- const Switcher = styled.span`
-  margin-top: 20px;
-  a {
-    color: #1d9bf0;
-  }
-`
+import { Input, Switcher, Title, Wrapper, Form, Error } from "../components/auth-components"
 
 export const CreateAccount = () => {
   const navigate = useNavigate()
