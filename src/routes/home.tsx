15e84ee9184d2e58f8eeadf06f2114918b98450c
NovaCat <sparkstorms2@gmail.com>
2025-04-10 23:09:22 +0900

    Feat: Building UI layout

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,21 +1,3 @@
-import { useNavigate } from "react-router-dom"
-import { auth } from "../utils/firebase"
-
 export const Home = () => {
-  const navigate = useNavigate()
-
-  const onClickLogout = async () => {
-    try {
-      await auth.signOut()
-      navigate("/login")
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  return (
-    <h1>
-      <button onClick={onClickLogout}>Logout</button>
-    </h1>
-  )
+  return <h1>Home</h1>
 }
