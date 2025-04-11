43118b3c8e9b36b4d7b34ea4716e5844764f2ee3
NovaCat <sparkstorms2@gmail.com>
2025-04-12 01:00:00 +0900

    Feat: fetch data realtime

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import styled from "styled-components"
 import { ITweet } from "../types"
-import { collection, getDocs, orderBy, query } from "firebase/firestore"
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore"
 import { db } from "../utils/firebase"
 import { DB_COLLECTION_PATH } from "../constants"
 import { Tweet } from "./tweet"
@@ -11,18 +11,20 @@ const Wrapper = styled.section``
 export const TimeLine = () => {
   const [tweets, setTweets] = useState<ITweet[]>([])
 
-  const fetchTweets = async () => {
+  const fetchTweets = () => {
     const tweetsQuery = query(
       collection(db, DB_COLLECTION_PATH),
       orderBy("createdAt", "desc")
     )
-    const snapshot = await getDocs(tweetsQuery)
-    const fetchedTweets = snapshot.docs.map((doc) => {
-      const { tweet, createdAt, userId, username, photo } = doc.data()
-      return { id: doc.id, tweet, createdAt, userId, username, photo }
-    })
 
-    setTweets(fetchedTweets)
+    onSnapshot(tweetsQuery, (snapshot) => {
+        const fetchedTweets = snapshot.docs.map(doc => {
+            const { tweet, createdAt, userId, username, photo } = doc.data()
+            return { id: doc.id, tweet, createdAt, userId, username, photo }
+        })
+
+        setTweets(fetchedTweets)
+    })
   }
 
   useEffect(() => {
