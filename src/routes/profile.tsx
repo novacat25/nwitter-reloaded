78879db692e96bd41f5bcd28a12e823b8cd83b8c
NovaCat <sparkstorms2@gmail.com>
2025-04-14 23:18:46 +0900

    Feat: update user's photo avatar

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -2,14 +2,40 @@ import styled from "styled-components"
 import { DEFAULT_NICKNAME } from "../constants"
 import { auth } from "../utils/firebase"
 import { useState } from "react"
+import { DefaultUserIcon } from "../components/icons/DefaultUserIcon"
 
-const Wrapper = styled.section``
-const AvatarUpload = styled.section``
-const AvatarImg = styled.img``
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`
+
+const AvatarUpload = styled.label`
+  width: 80px;
+  overflow: hidden;
+  height: 80px;
+  border-radius: 50%;
+  background-color: #1d9bf0;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    width: 50px;
+  }
+`
+
+const AvatarImg = styled.img`
+  width: 100%;
+`
+
 const AvatarInput = styled.input`
   display: none;
 `
-const Name = styled.span``
+const Name = styled.span`
+  font-size: 22px;
+`
 
 export const Profile = () => {
   const user = auth.currentUser
@@ -17,9 +43,10 @@ export const Profile = () => {
 
   return (
     <Wrapper>
-      <AvatarUpload>
+      <AvatarUpload htmlFor="avatar">
+        {avatar ? <AvatarImg src={avatar} /> : <DefaultUserIcon />}
       </AvatarUpload>
-      <AvatarInput type="file" accept="image/*" />
+      <AvatarInput id="avatar" type="file" accept="image/*" />
       <Name>
         {user?.displayName ? user.displayName : DEFAULT_NICKNAME}
       </Name>
