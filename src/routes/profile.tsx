84639afa1a6fb75c5dfd1f5e067b748b241701a5
NovaCat <sparkstorms2@gmail.com>
2025-04-14 21:55:24 +0900

    Feat: init(Profile page)

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,5 +1,28 @@
+import styled from "styled-components"
+import { DEFAULT_NICKNAME } from "../constants"
+import { auth } from "../utils/firebase"
+import { useState } from "react"
+
+const Wrapper = styled.section``
+const AvatarUpload = styled.section``
+const AvatarImg = styled.img``
+const AvatarInput = styled.input`
+  display: none;
+`
+const Name = styled.span``
+
 export const Profile = () => {
+  const user = auth.currentUser
+  const [avatar, setAvatar] = useState(user?.photoURL)
+
   return (
-    <h1>Profile</h1>
+    <Wrapper>
+      <AvatarUpload>
+      </AvatarUpload>
+      <AvatarInput type="file" accept="image/*" />
+      <Name>
+        {user?.displayName ? user.displayName : DEFAULT_NICKNAME}
+      </Name>
+    </Wrapper>
   )
 }
