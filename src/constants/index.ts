ec4a710cda1fe9f012c19333574cab6c14b54a28
NovaCat <sparkstorms2@gmail.com>
2025-04-14 23:38:15 +0900

    Feat: user timeline

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,6 @@
 export const DB_COLLECTION_PATH = "tweets"
+export const AVATAR_IMAGE_PATH = "avatars"
+
 export const DEFAULT_NICKNAME = "Anonymous"
 export const FETCH_LIMIT = 25
 export const TWEET_MAX_LENGTH = 200
--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components"
-import { DEFAULT_NICKNAME } from "../constants"
-import { auth } from "../utils/firebase"
-import { useState } from "react"
+import { AVATAR_IMAGE_PATH, DB_COLLECTION_PATH, DEFAULT_NICKNAME, FETCH_LIMIT, FILE_SIZE_LIMIT } from "../constants"
+import { auth, db, storage } from "../utils/firebase"
+import { useEffect, useState } from "react"
 import { DefaultUserIcon } from "../components/icons/DefaultUserIcon"
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
+import { updateProfile } from "firebase/auth"
+import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore"
+import { ITweet } from "../types"
+import { Tweet } from "../components/tweet"
 
 const Wrapper = styled.section`
   display: flex;
@@ -37,19 +42,77 @@ const Name = styled.span`
   font-size: 22px;
 `
 
+const Tweets = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`
+
 export const Profile = () => {
   const user = auth.currentUser
-  const [avatar, setAvatar] = useState(user?.photoURL)
+  const userPhotoURL = user?.photoURL
+  const [avatar, setAvatar] = useState(userPhotoURL)
+  const [tweets, setTweets] = useState<ITweet[]>([])
+  const fetchTweets = async () => {
+    const tweetsQuery = query(
+      collection(db, DB_COLLECTION_PATH),
+      where("userId", "==", user?.uid),
+      orderBy("createdAt", "desc"),
+      limit(FETCH_LIMIT)
+    )
+
+    const snapshot = await getDocs(tweetsQuery)
+    const tweets = snapshot.docs.map(doc => {
+      const { tweet, createdAt, userId, username, photo } = doc.data()
+      return { id: doc.id, tweet, createdAt, userId, username, photo }
+    })
+    setTweets(tweets)
+  }
+
+  useEffect(() => {
+    fetchTweets()
+  }, [tweets])
+  const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if(!user) return
+
+    const { files } = e?.target
+    
+    if(files && files.length === 1) {
+      const targetFile = files[0]
+        if(targetFile.size > FILE_SIZE_LIMIT) {
+          alert("File size is too big!")
+          setAvatar(userPhotoURL)
+          return
+        }
+
+        const locationRef = ref(storage, `${AVATAR_IMAGE_PATH}/${user.uid}`)
+        const uploadResult = await uploadBytes(locationRef, targetFile)
+        const avatarUrl = await getDownloadURL(uploadResult.ref)
+        const updatePhotoPayload = { photoURL: avatarUrl }
+        await updateProfile(user, updatePhotoPayload)
+        setAvatar(avatarUrl)
+      }    
+  }
 
   return (
     <Wrapper>
       <AvatarUpload htmlFor="avatar">
         {avatar ? <AvatarImg src={avatar} /> : <DefaultUserIcon />}
       </AvatarUpload>
-      <AvatarInput id="avatar" type="file" accept="image/*" />
+      <AvatarInput 
+        id="avatar" 
+        type="file" 
+        accept="image/*" 
+        onChange={onAvatarChange} 
+      />
       <Name>
         {user?.displayName ? user.displayName : DEFAULT_NICKNAME}
       </Name>
+      <Tweets>
+        {tweets.map((tweet) => (
+          <Tweet key={tweet.id} {...tweet} />
+        ))}
+      </Tweets>
     </Wrapper>
   )
 }
